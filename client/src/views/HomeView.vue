2a1594f8f3898b54aa2719b132dfaf4b3a255c49
quooctrangg <nguyenquoctrang188@gmail.com>
2023-12-31 09:02:13 +0700

    init

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+console.log(import.meta.env.VITE_PORT);
+</script>
+
+<template>
+    <h1 class="text-red-500">Hello</h1>
+</template>
---
+++ server/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      PORT: Joi.number().required(),
+    }),
+  })]
+})
+export class AppModule { }
