a64f26bd6872e380bd0f12d908a5cc398bbaafb8
quooctrangg <nguyenquoctrang188@gmail.com>
2024-03-11 22:57:55 +0700

    test python

--- server/src/suggest/python.config.ts
+++ server/src/suggest/python.config.ts
@@ -5,7 +5,7 @@ const logger = new Logger('Python Shell');
 
 export const config: Options = {
     mode: 'json',
-    pythonPath: 'C:/Program Files/Python312/python.exe',
+    pythonPath: 'C:/Program Files/Python311/python.exe',
     scriptPath: './src/suggest/scripts',
     pythonOptions: ['-u'],
     stderrParser: (log) => logger.verbose(log)
--- server/src/suggest/scripts/main.py
+++ server/src/suggest/scripts/main.py
@@ -1,7 +1,28 @@
-import json
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
-x =  '{ "name":"John", "age":30, "city":"New York"}'
+data = pd.read_csv("data.csv")
 
-y = json.loads(x)
+df = pd.DataFrame(data)
 
-print(y["age"])
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['description'])
+
+similarities = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+new_description = "CCCD"
+
+new_description_vector = tfidf_vectorizer.transform([new_description])
+
+similarities_new = cosine_similarity(new_description_vector, tfidf_matrix)
+
+n_similar_items = 2
+
+similar_items_indices = similarities_new.argsort(axis=1).squeeze()[-n_similar_items:][::-1]
+
+similar_items_ids = df.loc[similar_items_indices, 'itemId'].tolist()
+
+print("Các itemId được đề xuất dựa trên mô tả đã nhập:")
+
+print(similar_items_ids)
