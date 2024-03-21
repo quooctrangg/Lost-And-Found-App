0f8f185709ec4fe8ad4ff5616fe16062774a0b1a
quooctrangg <nguyenquoctrang188@gmail.com>
2024-03-22 01:41:59 +0700

    smooth scroll to top

--- client/src/components/common/ScrollToTop.vue
+++ client/src/components/common/ScrollToTop.vue
@@ -1,11 +1,15 @@
 <script setup>
 function scrollToTop() {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
 </script>
 
 <template>
-    <div class="fixed text-2xl bottom-10 right-10 px-3 py-1 rounded-full bg-green-200 cursor-pointer" @click="scrollToTop">
+    <div class="fixed text-2xl bottom-10 right-10 px-3 py-1 rounded-full bg-green-200 cursor-pointer"
+        @click="scrollToTop">
         <i class="fa-solid fa-arrow-up"></i>
     </div>
 </template>
