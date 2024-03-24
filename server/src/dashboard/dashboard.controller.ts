9907c8f17b972189630f7d9012926a6c02b04153
quooctrangg <nguyenquoctrang188@gmail.com>
2024-03-24 23:52:53 +0700

    add api download xlsx

--- server/src/dashboard/dashboard.controller.ts
+++ server/src/dashboard/dashboard.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Header, Query, Res, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { MyJWTGuard, RolesGuard } from '../auth/guard';
 import { Role } from '../auth/decorator';
 import { USER_TYPES } from '../global';
+import type { Response } from 'express'
+import * as moment from 'moment-timezone';
 
 @Controller('dashboard')
-@UseGuards(MyJWTGuard, RolesGuard)
+// @UseGuards(MyJWTGuard, RolesGuard)
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) { }
 
@@ -26,4 +28,16 @@ export class DashboardController {
   getListStudentRetureItemSuccessful(@Query() option: { type: string, month: string, year: string, to: string, from: string }) {
     return this.dashboardService.getListStudentRetureItemSuccessful(option)
   }
+
+  @Get('download-excel')
+  // @Role(USER_TYPES.ADMIN)
+  async downloadExcel(@Res({ passthrough: true }) res: Response, @Query() option: { type: string, month: string, year: string, to: string, from: string }) {
+    const { data, name } = await this.dashboardService.downloadExcel(option)
+    res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    res.setHeader("Content-Disposition", "attachment; filename=" + `Danh sach ${name}.xlsx`)
+    return data.xlsx.write(res).then(function () {
+      res.status(200).end();
+    });
+  }
+
 }
--- server/src/dashboard/dashboard.service.ts
+++ server/src/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ResponseData } from '../global';
+import { Workbook } from 'exceljs'
 import * as moment from 'moment-timezone';
 
 @Injectable()
@@ -161,7 +162,7 @@ export class DashboardService {
           end = new Date(endDate.clone().utc().format())
           break;
       }
-      const list: { lost: string, found: string, item: string, school: string, sendProtection: boolean }[] = []
+      let list: { lost: string, found: string, item: string, major: string, marjoId?: number, day: Date, school: string, schoolId?: number }[] = []
       const lostandfond = await this.prismaService.request.findMany({
         where: {
           status: 1,
@@ -175,7 +176,11 @@ export class DashboardService {
             select: {
               id: true,
               email: true,
-              Major: true
+              Major: {
+                include: {
+                  School: true
+                }
+              }
             },
           },
           Post: {
@@ -184,7 +189,11 @@ export class DashboardService {
                 select: {
                   id: true,
                   email: true,
-                  Major: true
+                  Major: {
+                    include: {
+                      School: true
+                    }
+                  }
                 }
               },
               Item: true
@@ -206,7 +215,11 @@ export class DashboardService {
             select: {
               id: true,
               email: true,
-              Major: true
+              Major: {
+                include: {
+                  School: true
+                }
+              }
             }
           },
           Item: true
@@ -214,17 +227,186 @@ export class DashboardService {
       })
       lostandfond.forEach(e => {
         if (e.Post.type == true) {
-          list.push({ found: e.Post.User.email, school: e.Post.User.Major.name, lost: e.User.email, item: e.Post.Item.name, sendProtection: false })
+          list.push({ found: e.Post.User.email, school: e.Post.User.Major.School.name, major: e.Post.User.Major.name, lost: e.User.email, item: e.Post.Item.name, day: e.updatedAt, marjoId: e.Post.User.Major.id, schoolId: e.Post.User.Major.schoolId })
         } else {
-          list.push({ found: e.User.email, school: e.User.Major.name, lost: e.Post.User.email, item: e.Post.Item.name, sendProtection: false })
+          list.push({ found: e.User.email, school: e.User.Major.School.name, major: e.User.Major.name, lost: e.Post.User.email, item: e.Post.Item.name, day: e.updatedAt, marjoId: e.User.Major.id, schoolId: e.User.Major.schoolId })
         }
       })
       sendProtection.forEach(e => {
-        list.push({ found: e.User.email, school: e.User.Major.name, lost: '', item: e.Item.name, sendProtection: true })
+        list.push({ found: e.User.email, school: e.User.Major.School.name, major: e.User.Major.name, lost: '', item: e.Item.name, day: e.updatedAt, marjoId: e.User.Major.id, schoolId: e.User.Major.schoolId })
       })
+      list = list.sort((a, b) => {
+        if (a.schoolId < b.schoolId) return 1
+        if (a.schoolId > b.schoolId) return -1
+        return a.marjoId - b.marjoId
+      })
+      list = list.map(({ schoolId, marjoId, ...rest }) => rest)
       return new ResponseData<any>(list, 200, 'Thống kê')
     } catch (error) {
       return new ResponseData<string>(null, 500, 'Lỗi dịch vụ, thử lại sau')
     }
+  }
+
+  async downloadExcel(option: { type: string, month: string, year: string, to: string, from: string }) {
+    let start: any
+    let end: any
+    let list: { lost: string, found: string, item: string, major: string, marjoId?: number, day: string, school: string, schoolId?: number }[] = []
+    let rows: any[] = []
+    const { type, month, year, to, from } = option
+    switch (type) {
+      case 'month':
+        start = new Date(Number(year), Number(month) - 1, 1)
+        end = new Date(Number(year), Number(month), 0);
+        break;
+      case 'year':
+        start = new Date(Number(year), 0, 1)
+        end = new Date(Number(year), 12, 0)
+        break;
+      case 'any':
+        const startDate = moment.tz(to, 'Asia/Ho_Chi_Minh');
+        const endDate = moment.tz(from, 'Asia/Ho_Chi_Minh');
+        start = new Date(startDate.clone().utc().format())
+        end = new Date(endDate.clone().utc().format())
+        break;
+    }
+    const lostandfond = await this.prismaService.request.findMany({
+      where: {
+        status: 1,
+        createdAt: {
+          gte: start,
+          lte: end
+        }
+      },
+      include: {
+        User: {
+          select: {
+            id: true,
+            email: true,
+            Major: {
+              include: {
+                School: true
+              }
+            }
+          },
+        },
+        Post: {
+          include: {
+            User: {
+              select: {
+                id: true,
+                email: true,
+                Major: {
+                  include: {
+                    School: true
+                  }
+                }
+              }
+            },
+            Item: true
+          }
+        }
+      }
+    })
+    const sendProtection = await this.prismaService.post.findMany({
+      where: {
+        done: -1,
+        updatedAt: {
+          gte: start,
+          lte: end
+        },
+        verify: 1
+      },
+      include: {
+        User: {
+          select: {
+            id: true,
+            email: true,
+            Major: {
+              include: {
+                School: true
+              }
+            }
+          }
+        },
+        Item: true
+      }
+    })
+    lostandfond.forEach(e => {
+      if (e.Post.type == true) {
+        list.push({ found: this.getMSSV(e.Post.User.email), school: e.Post.User.Major.School.name, major: e.Post.User.Major.name, lost: this.getMSSV(e.User.email), item: e.Post.Item.name, day: moment(e.updatedAt).format('DD-MM-YYYY'), marjoId: e.Post.User.Major.id, schoolId: e.Post.User.Major.schoolId })
+      } else {
+        list.push({ found: this.getMSSV(e.User.email), school: e.User.Major.School.name, major: e.User.Major.name, lost: this.getMSSV(e.Post.User.email), item: e.Post.Item.name, day: moment(e.updatedAt).format('DD-MM-YYYY'), marjoId: e.User.Major.id, schoolId: e.User.Major.schoolId })
+      }
+    })
+    sendProtection.forEach(e => {
+      list.push({ found: this.getMSSV(e.User.email), school: e.User.Major.School.name, major: e.User.Major.name, lost: '', item: e.Item.name, day: moment(e.updatedAt).format('DD-MM-YYYY'), marjoId: e.User.Major.id, schoolId: e.User.Major.schoolId })
+    })
+    list = list.sort((a, b) => {
+      if (a.schoolId < b.schoolId) return 1
+      if (a.schoolId > b.schoolId) return -1
+      return a.marjoId - b.marjoId
+    })
+    list = list.map(({ schoolId, marjoId, ...rest }) => rest)
+    list.forEach(doc => {
+      rows.push(Object.values(doc))
+    })
+    let book = new Workbook()
+    let sheet = book.addWorksheet('DanhSach')
+    rows.unshift(['Người tìm thấy', 'Trường / khoa', 'Chuyên ngành', 'Người thất lạc', 'Loại đồ', 'Ngày'])
+    rows.unshift([''])
+    rows.unshift(['Danh sách các sinh viên nhặt và trả lại thành công'])
+    sheet.addRows(rows)
+    sheet.getColumn(1).width = 20.5
+    sheet.getColumn(2).width = 50
+    sheet.getColumn(3).width = 40
+    sheet.getColumn(4).width = 20.5
+    sheet.getColumn(5).width = 20.5
+    sheet.getColumn(6).width = 20.5
+    sheet.mergeCells(1, 1, 1, 6)
+    sheet.getCell('A1').alignment = { vertical: 'middle', horizontal: 'center' }
+    sheet.getCell('A1').font = { size: 20, bold: true };
+    for (let i = 0; i < 6; i++) {
+      sheet.getCell(3, i + 1).font = { size: 13, bold: true }
+      sheet.getCell(3, i + 1).border = {
+        top: { style: 'thin' },
+        left: { style: 'thin' },
+        bottom: { style: 'thin' },
+        right: { style: 'thin' }
+      };
+    }
+    for (let j = 4; j <= rows.length; j++) {
+      for (let i = 0; i < 6; i++) {
+        sheet.getCell(j, i + 1).border = {
+          top: { style: 'thin' },
+          left: { style: 'thin' },
+          bottom: { style: 'thin' },
+          right: { style: 'thin' }
+        };
+      }
+    }
+    return { data: book, name: this.setDate(option) }
+  }
+
+  getMSSV(email: string) {
+    const regex = /B\d{7}/i;
+    const match = email.match(regex);
+    return match ? match[0] : null;
+  }
+
+
+  setDate(option) {
+    let result = ''
+    switch (option.type) {
+      case 'month':
+        result = `Thang ${option.month}-${option.year}`
+        break;
+      case 'year':
+        result = `Nam ${option.year}`
+        break;
+      case 'any':
+        result = `Tu ngay ${moment(option.to).format('DD-MM-YYYY')} đen ${moment(option.from).format('DD-MM-YYYY')}`
+        break;
+    }
+    return result
   }
 }
